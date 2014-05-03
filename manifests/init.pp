c999ce4dd9abc513e46a1edb79f1b27c1d8c80a8
Alex Harvey <alexharv074@gmail.com>
2014-05-03 11:40:27 +0000

    Add smtp.routes file and qmailctl cdb command.

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,12 +5,14 @@ class qmail (
   $rcpthosts       = $::qmail::params::rcpthosts,
   $smtproutes      = $::qmail::params::smtproutes,
   $locals          = $::qmail::params::locals,
+  $tcp_smtp        = $::qmail::params::tcp_smtp,
 
 ) inherits ::qmail::params {
 
   validate_array($rcpthosts)
   validate_array($smtproutes)
   validate_array($locals)
+  validate_array($tcp_smtp)
 
   # N.B. Qmail package on Ubuntu adds qmail users in
   # the post install script.  Ubuntu is the only platform
@@ -26,6 +28,7 @@ class qmail (
   $rcpthosts_content  = inline_template('<%= @rcpthosts.join("\n")  + "\n" %>')
   $smtproutes_content = inline_template('<%= @smtproutes.join("\n") + "\n" %>')
   $locals_content     = inline_template('<%= @locals.join("\n")     + "\n" %>')
+  $tcp_smtp_content   = inline_template('<%= @tcp_smtp.join("\n")   + "\n" %>')
 
   File {
     owner   => 'root',
@@ -59,4 +62,13 @@ class qmail (
     content => $locals_content,
   }
 
+  file { '/etc/qmail/tcp.smtp':
+    ensure  => file,
+    content => $tcp_smtp_content,
+  }
+
+  exec { '/usr/bin/qmailctl cdb':
+    subscribe   => File['/etc/qmail/tcp.smtp'],
+    refreshonly => true,
+  }
 }
